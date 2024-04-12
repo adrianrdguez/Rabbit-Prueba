4256a77f488e6ac9b7319e907b708dd14e9a0a3b
adrianrdguez <60879085+adrianrdguez@users.noreply.github.com>
2024-04-12 10:53:38 +0100

    Update store-info.form.ts

--- store-info.form.ts
+++ store-info.form.ts
@@ -4,11 +4,16 @@ import { Store } from "../../models/store/store";
 import { IdName } from "@shared/models/classes/id-name";
 import { map } from "rxjs/operators";
 import { Observable } from "rxjs";
+import { ListOption } from "@shared/models/interfaces/list-option";
 
 @Injectable({ providedIn: 'root' })
 export class StoreInfoFormService {
   private store: Store = new Store();
   isNew?: boolean;
+
+  regionsListOptions: ListOption[] = [];
+  provincesListOptions: ListOption[] = [];
+  citiesListOptions: ListOption[] = [];
 
   public storeInformationForm: FormGroup = new FormGroup({
     uid: new FormControl({ value: this.store.id, disabled: true }),
@@ -37,12 +42,13 @@ export class StoreInfoFormService {
     this.handleSubmitionForm(newStore);
     return newStore;
   }
-  
+
   public get isAllValid$(): Observable<boolean> {
     return this.storeInformationForm.valueChanges.pipe(
       map(() => this.storeInformationForm.valid)
     );
   }
+
   public get isAllValid(): boolean {
     return this.storeInformationForm.valid;
   }
@@ -63,22 +69,34 @@ export class StoreInfoFormService {
       latitude: store.latitude,
       currency: store.currency?.id,
     });
+
     this.storeInformationForm.markAsPristine();
   }
 
+
+
   private handleSubmitionForm(store: Store) {
-    const regionId = this.storeInformationForm.get('region')?.value;
-    const provinceId = this.storeInformationForm.get('province')?.value;
-    const cityId = this.storeInformationForm.get('city')?.value;
+    const region = this.regionsListOptions.find(
+      (region) => region.key === this.storeInformationForm.get('region')?.value
+    ) ?? {} as any;
+
+    const province  = this.provincesListOptions.find
+    ((province) => province.key === this.storeInformationForm.get('province')?.value
+    ) ?? {} as any;
+
+    const city = this.citiesListOptions.find(
+      (city) => city.key === this.storeInformationForm.get('city')?.value
+    )  ?? {} as any;
+
     const currencyId = this.storeInformationForm.get('currency')?.value;
 
     store.id = this.storeInformationForm.get('uid')?.value;
     store.storecode = this.storeInformationForm.get('storecode')?.value;
     store.name = this.storeInformationForm.get('name')?.value;
-    store.region = IdName.buildOrUndefined({ id: regionId });
-    store.address = this.storeInformationForm.get('streetAddress')?.value;
-    store.city = IdName.buildOrUndefined({ id: cityId });
-    store.province = IdName.buildOrUndefined({ id: provinceId });
+    store.region = IdName.buildOrUndefined({ id: region.key, name: region.value });
+    store.address = this.storeInformationForm.get('address')?.value;
+    store.city = IdName.buildOrUndefined({ id: city.key, name: city.value });
+    store.province = IdName.buildOrUndefined({ id: province.key, name: province.value });
     store.postalCode = this.storeInformationForm
       .get('postalCode')
       ?.value?.toString();
